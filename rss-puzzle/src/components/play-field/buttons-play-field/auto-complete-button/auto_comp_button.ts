d0c42ff8b3a0f3466120ca6bf560d36e6a069d06
Yana-Dyachok <annetart1994@gmail.com>
2024-07-01 19:50:14 +0300

    feat: add deploy's files

--- rss-puzzle/src/components/play-field/buttons-play-field/auto-complete-button/auto_comp_button.ts
+++
@@ -1,9 +0,0 @@
-import Button from '../../../button/button';
-
-class AutoCompleteButton extends Button {
-    constructor() {
-        super('auto-complete_btn', 'Auto-Complete');
-    }  
-}
-
-export default AutoCompleteButton;
--- rss-puzzle/src/index.ts
+++
@@ -1,5 +0,0 @@
-import App from './components/app/app';
-import './styles/style.scss';
-
-const app = new App();
-app.start();
